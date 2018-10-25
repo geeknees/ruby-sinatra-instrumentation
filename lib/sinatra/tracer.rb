b2547d878f2718adff18c65d2385311e583b5173
Ashwin Chandrasekar <achandrasekar@signalfx.com>
2018-10-25 14:08:27 -0400

    Integrate Rack::Tracer for routes and patch the base render method

--- lib/sinatra/tracer.rb
+++ lib/sinatra/tracer.rb
@@ -1,7 +1,39 @@
-require "sinatra/tracer/version"
+require 'rack/tracer'
+require 'sinatra/base'
+require 'sinatra/tracer/version'
+require 'opentracing'
 
 module Sinatra
   module Tracer
-    # Your code goes here...
+
+    class << self
+
+      def registered(app)
+        # enable the Rack Tracer middleware
+        app.use Rack::Tracer
+
+        patch_render
+      end
+
+      def patch_render
+        ::Sinatra::Base.module_eval do
+          alias_method :render_old, :render
+
+          def render(engine, data, options = {}, locals = {}, &block)
+            result = ""
+
+            OpenTracing.global_tracer.start_active_span("Rendering View") do |scope|
+              result = render_old(engine, data, options, locals, &block)
+              scope.span.set_tag("sinatra.template", data)
+            end
+
+            result
+          end
+        end
+      end
+
+    end
   end
+
+  register Tracer
 end
